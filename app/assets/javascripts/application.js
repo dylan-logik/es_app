4b7a5a5a3cb3e98173c544f7fca402a1cf521b04
Tucker <tucker@logik.com>
2012-03-20 23:53:29 -0400

    highcharts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,7 @@
 //
 //= require twitter/bootstrap
 //= require jquery.dateFormat-1.0
+//= require highcharts
 //= require highstock
 //
 //= require es_app
--- app/assets/javascripts/views/search/search_charts.js
+++ app/assets/javascripts/views/search/search_charts.js
@@ -9,7 +9,7 @@ ESApp.Views.SearchCharts = Support.CompositeView.extend({
   },
 
   render: function() {
-    this.$el.html(JST['search/charts']());
+    //this.$el.html(JST['search/charts']());
     this.renderChart();
     return this;
   },
@@ -18,16 +18,54 @@ ESApp.Views.SearchCharts = Support.CompositeView.extend({
     var self = this;
     this.collection.each(function(facet) {
       if (facet.get('_type') == 'date_histogram') {
+        self.$el.append("<div class='span12' id='" + facet.get('name').replace(/\./g, '-') + "'></div>");
         self.buildChart(facet);
+      } else if (facet.get('_type') == 'terms') {
+        self.$el.append("<div class='span6' id='" + facet.get('name').replace(/\./g, '-') + "'></div>");
+        self.buildPie(facet);
       }
     });
   },
-  
+ 
+  buildPie: function(facet) {
+    return new Highcharts.Chart({
+      chart: {
+        renderTo: this.$('#'+facet.get('name').replace(/\./g, '-'))[0],
+        plotBackgroundColor: null,
+        plotBorderWidth: null
+      },
+      title: {
+        text: facet.get('name')
+      },
+      plotOptions: {
+        pie: {
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            color: '#000000',
+            connectorColor: '#000000',
+            formatter: function() {
+              return '<b>'+ this.point.name +'</b>: '+ this.percentage.toFixed(2) +' %';
+            }
+          }
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+      series: [{
+        type: 'pie',
+        name: facet.get('name'),
+        data: this.preprocessTerms(facet.get('terms'), facet.get('total'))
+      }]
+    });
+  },
+ 
   buildChart: function(facet) {
     var self = this;
     return new Highcharts.StockChart({
       chart: {
-        renderTo: self.$('#created_at')[0],
+        renderTo: self.$('#'+facet.get('name').replace('.', '-'))[0],
         events: {
           redraw: function() {
             console.debug(this.xAxis[0].getExtremes());
@@ -56,6 +94,16 @@ ESApp.Views.SearchCharts = Support.CompositeView.extend({
   preprocess: function(data) {
     return _.map(data, function(entry) {
       return [ entry.time, entry.count ];
+    });
+  },
+
+  preprocessTerms: function(terms) {
+    var total = _.reduce(_.pluck(terms, 'count'), 
+        function(memo, count) {
+          return memo + count;
+        }, 0);
+    return d = _.map(terms, function(term) {
+      return [term.term, term.count / total];
     });
   }
 
