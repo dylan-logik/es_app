13f692fa8d208c2de964983612abb4435ab154f4
Tucker <tucker@logik.com>
2012-06-04 09:10:34 -0400

    Updated mapping

--- mapping.sh
+++ mapping.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 curl -XPUT "http://localhost:9200/tweets/tweet/_mapping" -d '
-"tweet": {
-  "properties": {
-    "tags": {
-      "type": "multi_field",
-      "fields": {
-        "tags": {
-          "type": "string"
-        },
-        "untouched": {
-          "index": "not_analyzed",
-          "type": "string",
-        }
-      }
-    },
-    "user": {
-      "properties": {
-      "name": {
+{
+  "tweet": {
+    "properties": {
+      "tags": {
         "type": "multi_field",
         "fields": {
-          "name": {
-            "type": "string"
-          }
+          "tags": { "type": "string" },
           "untouched": {
             "index": "not_analyzed",
             "type": "string"
           }
         }
+      },
+      "user": {
+        "properties": {
+          "name": {
+            "type": "multi_field",
+            "fields": {
+              "name": { "type": "string" },
+              "untouched": {
+                "index": "not_analyzed",
+                "type": "string"
+              }
+            }
+          }
+        }
       }
     }
   }
-}
-'
+}'
